459c4f62e57ccae9e3703d176f18f9e4173b915c
Zeitsperre <trevor_smith@live.com>
2018-01-22 15:04:25 -0500

    Added a method for year-sorting station list matches

--- ec_stations_daily.py
+++ ec_stations_daily.py
@@ -105,27 +105,65 @@ def grabThat(station):
                 value = value
                 datum.setdefault(column, []).append(value)
         return datum
+        
+def matchLocations(locations):
+    '''
+    A method to match locations that appear multiple times in the same list
+    and return year-ordered lists that can then be plotted sequentially
+    '''
+    
+    ident = 'Climate Identifier'
+    year = 'Year'
 
-def outputThat(locations):
+    matches = []
+    order_years = [[]]
+    processed_stations = []
+    
+    for i, station1 in enumerate(locations):
+        if station1[ident] in processed_stations:
+            continue
+        matches.append([])
+        matches[-1].append(station1)
+        order_years[-1].append(int(station1[year][0]))
+        for station2 in locations[i+1:]:
+            if station1[ident] == station2[ident]\
+               and int(station1[year][0]) != int(station2[year][0]):
+                matches[-1].append(station2)
+                order_years[-1].append(int(station2[year][0]))
+        processed_stations.append(station1[ident])
+        matches[-1] = [x for _,x in sorted(zip(order_years[-1], matches[-1]))]
+
+    return matches
+
+def calcThat(match, plot):
     '''   
-    A method that converts a unicode dictionary of climate data
-    to ASCII and proceeds to create a numpy plot with a dataframe 
+    A method that converts a unicode dictionary of 
+    climate data to ASCII and proceeds to create a dataframe 
     derived from it.
     '''
-    dataframe = locations
-    for key in locations:
+    location = match
+    for key in match:
         newkey = key.encode('ascii','ignore')
-        dataframe[newkey] = dataframe.pop(key)
-        for count, val in enumerate(locations[newkey]):
+        location[newkey] = location.pop(key)
+        for count, val in enumerate(match[newkey]):
             try:
                 newval = val.encode('ascii','ignore')
-                dataframe[newkey][count] = newval
+                location[newkey][count] = newval
             except TypeError:
                 continue
     
-    def outputDD10(dataframe):
+    def outpMinT(dataframe):
+        pass
+    
+    def outMaxT(dataframe):
+        pass
+    
+    def outPpt(dataframe):
+        pass
+    
+    def outDD10(dataframe, plot):
         '''
-        Calculates and creates a plot and value indicators for degree-days
+        Calculates andformats value indicators for degree-days
         with base 10 Celsius
         '''
         def degreeDays(low, high):
@@ -147,58 +185,80 @@ def outputThat(locations):
         min_temp_masked = np.ma.masked_where(min_temp_raw == '', min_temp_raw)    
         min_temp = np.array([float(val) for val in min_temp_masked])
 
-    
         x = np.arange(0.,len(period))
-        y1 = map(degreeDays, min_temp, max_temp)
-        max_daily, sum_daily = max(y1), np.nansum(y1)
+        y = map(degreeDays, min_temp, max_temp)
+        max_daily, sum_daily = max(y), np.nansum(y)
         max_daily_title = 'Maximum Daily Value: {}'.format(max_daily)
         sum_daily_title = 'Sum of Daily Values: {}'.format(sum_daily)
-        
-        plt.plot(x, y1, 'o')
-        plt.title('Degree Days (Base 10C) for year {}'.format(year))
-        plt.xlabel('Day of Year')
-        plt.ylabel('Degree-Days (Base 10C)')
-        
-        plt.annotate(max_daily_title, xy=(30, max_daily))
-        plt.annotate(sum_daily_title, xy=(200, max_daily))
-
-        plt.show()
+      
+        return x, y, year, max_daily_title, sum_daily_title 
+ 
+    dd10 = outDD10(location, plot) 
+    return dd10
     
-        return None
-        
-    outputDD10(dataframe)
+def plotThat(plot, x, va, year, maxtitle, sumtitle):
+    '''
+    Using plot names, axes, year and titles, creates a sublot
+    '''
     
-
+#    fig, ax = plt.subplots(plot,1)
+#    
+#    plot.plot(x, va, 'o')
+#    plot.title('Degree Days (Base 10C) for year {}'.format(year))
+#    plot.xlabel('Day of Year')
+#    plot.ylabel('Degree-Days (Base 10C)')
+#    
+#    plot.annotate(max_daily_title, xy=(30, max_daily))
+#    plot.annotate(sum_daily_title, xy=(200, max_daily))
+#    plt.show()    
+    
+    pass
+    
+    
+    
 def main():
     '''
     The main script that calls other methods; Odd calls and methods that are
     yet to be integrated are placed here to ensure script methods can be run to
     completion before they are used by other scripts.
     '''
-    ec_stations = collectThat()
+    fnames = collectThat()
     locations = []    
     
-    if len(ec_stations) > 1:
-        for station in ec_stations:
-            place = placeThat(station)
+    if len(fnames) > 1:
+        for f in fnames:
+            place = placeThat(f)
             locations.append(place)
-        print len(ec_stations), 'stations gathered'
+        print len(fnames), 'stations gathered'
     else:
-        place = placeThat(ec_stations[0])
+        place = placeThat(fnames[0])
         locations.append(place)
         print 'Single station gathered'
 
-    for count, station in enumerate(ec_stations):
+    for count, station in enumerate(fnames):
         datum = [] 
-        datum = grabThat(station) 
-        locations[count].update(datum)    
+        datum = grabThat(station)
+        locations[count].update(datum)
     
-    if len(locations) > 1:
-        for count, datum in enumerate(locations):
-            plt.subplot(2,1,1)
-            outputThat(locations[count])
-    else:
-        outputThat(locations)
+    matches = matchLocations(locations)
+
+#    for match in matches:
+#        if match >1:
+#            plots = len(match)
+#            ax = []
+#            for count in match:
+#                ax = 'ax'+str(count)
+#
+#            for count, plots in enumerate(ax):
+#                plot = 'ax'+str(count)
+#                
+#                calcThat(match[count], plot)
+#        else:
+#            pass
+#    
+#    plt.show()    
+#    
+    return None
 
 if __name__ == "__main__":
     '''
